467699005656719237bf6a9f6f625cd0fe23ef5c
rickkas7 <rickk@rickk.com>
2023-08-27 12:29:22 -0400

    checkpoint

--- firmware/src/udptester.cpp
+++ firmware/src/udptester.cpp
@@ -10,6 +10,8 @@ bool wifiReady = false;
 IPAddress localIP;
 const uint16_t localPort = 8200;
 UDP udp;
+uint8_t packetBuf[10000];
+int nextSeq = 0;
 
 void setup() {
 
@@ -30,6 +32,23 @@ void loop() {
             });
 
             udp.begin(localPort);
+        }
+
+        int size = udp.receivePacket(packetBuf, sizeof(packetBuf));
+        if (size >= 0) {
+            if (size >= 4) {
+                int seq = *(int *)packetBuf;
+                if (seq == nextSeq) {
+                    Log.info("packet seq=%d size=%d", seq, size);
+                }
+                else {
+                    Log.info("packet seq=%d size=%d, expected seq=%d, delta=%d", seq, size, nextSeq, seq - nextSeq);
+                }
+                nextSeq = seq + 1;
+            }
+            else {
+                Log.info("packet size=%d", size);
+            }
         }
     }
     else {
@@ -55,3 +74,43 @@ void ctrl_request_custom_handler(ctrl_request *req) {
     system_ctrl_set_result(req, result, nullptr, nullptr, nullptr);
 }
 
+
+REGISTER_CONTROL_REQUEST_HANDLER([](ControlRequestState &state, JSONValue &outerObj, JSONBufferWriter &writer) {
+    int result = SYSTEM_ERROR_NOT_SUPPORTED;
+
+    if (state.op == "info") {
+        Log.info("info");
+        writer.name("wifi").value(WiFi.ready());
+        if (localIP) {
+            writer.name("ip").value(localIP.toString().c_str());
+            writer.name("port").value(localPort);
+        }
+        result = SYSTEM_ERROR_NONE;
+    }
+
+    return result;
+});
+
+
+REGISTER_CONTROL_REQUEST_HANDLER([](ControlRequestState &state, JSONValue &outerObj, JSONBufferWriter &writer) {
+    int result = SYSTEM_ERROR_NOT_SUPPORTED;
+
+    if (state.op == "start") {
+        Log.info("start");
+        result = SYSTEM_ERROR_NONE;
+    }
+
+    return result;
+});
+
+
+REGISTER_CONTROL_REQUEST_HANDLER([](ControlRequestState &state, JSONValue &outerObj, JSONBufferWriter &writer) {
+    int result = SYSTEM_ERROR_NOT_SUPPORTED;
+
+    if (state.op == "stop") {
+        Log.info("stop");
+        result = SYSTEM_ERROR_NONE;
+    }
+
+    return result;
+});
--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
 const helper = require('@rickkas7/particle-node-cli-helper');
 helper
@@ -5,15 +7,26 @@ helper
 
 const usb = require('particle-usb');
 
+let settings = {
+    interval: 1000, // milliseconds
+    size: 1024, // bytes of UDP payload
+    port: 8200,
+};
+
+let usbDevice;
+let runTimer;
+let remoteAddr;
+let remotePort;
+let sendSeq = 0;
 
 async function scanForDevices() {
     let usbDevice;
-    
+
     const scanOptions = {
         showQuitOption: true,
     };
 
-    while(true) {
+    while (true) {
         const devices = await usb.getDevices();
         if (devices.length == 1) {
             usbDevice = devices[0];
@@ -22,13 +35,13 @@ async function scanForDevices() {
         else if (devices.length > 0) {
             const deviceMenu = [];
 
-            for(const device of devices) {
+            for (const device of devices) {
                 await device.open();
                 deviceMenu.push(device.type + ' (' + device.id + ')');
                 await device.close();
             }
-        
-            const res = await helper.questionMenu('Select device: ', deviceMenu, scanOptions);            
+
+            const res = await helper.questionMenu('Select device: ', deviceMenu, scanOptions);
             usbDevice = devices[res];
 
             break;
@@ -38,44 +51,61 @@ async function scanForDevices() {
                 'Scan again',
             ];
 
-            await helper.questionMenu('No USB devices found. Scan again? ', scanAgainMenu, scanOptions);            
+            await helper.questionMenu('No USB devices found. Scan again? ', scanAgainMenu, scanOptions);
         }
     }
-    
+
     return usbDevice;
 }
 
+async function sendControlRequest(reqObj) {
+    try {
+        const res = await usbDevice.sendControlRequest(10, JSON.stringify(reqObj));
+
+        if (res.result == 0 && typeof res.data !== 'undefined') {
+            const json = JSON.parse(res.data);
+            return json;
+        }
+    }
+    catch (e) {
+        console.log('control request exception', e);
+    }
+
+    return null;
+}
+
+async function stopTest() {
+    if (runTimer) {
+        clearInterval(runTimer);
+        runTimer = null;
+
+        await sendControlRequest({ op: 'stop' });
+    }
+}
+
 async function run() {
-    const usbDevice = await scanForDevices();
+    usbDevice = await scanForDevices();
     await usbDevice.open();
 
     helper.rl.output.write('Opened Particle device ' + usbDevice.type + ' (' + usbDevice.id + ') via USB\n');
-    
-    let msgTimer;
-    msgTimer = setInterval(async function() {
-    
-        try {
-            let reqObj = {
-                op: 'msg'
-            };
-            const res = await usbDevice.sendControlRequest(10, JSON.stringify(reqObj));
 
-            if (res.result == 0 && typeof res.data !== 'undefined') {
-                console.log('res', res);
-                //const json = JSON.parse(res.data);
-                //console.log('msg', json);
+    let msgTimer;
+    msgTimer = setInterval(async function () {
+
+        const res = await sendControlRequest({ op: 'msg' });
+        if (res) {
+            console.log('msg', res);
+            if (typeof res.wifi != 'undefined') {
+                if (res.wifi) {
+                    remoteAddr = res.ip;
+                    remotePort = res.port;
+                }
+                else {
+                    remoteAddr = remotePort = null;
+                }
             }
         }
-        catch(e) {
-            console.log('control request exception', e);
-            /*
-            if (msgTimer) {
-                clearInterval(msgTimer);
-                msgTimer = null;
-            }
-            */
-        }
-    
+
     }, 1000);
 
     const testOptions = {
@@ -84,34 +114,59 @@ async function run() {
 
     const testCommands = [
         {
-            'title': 'Set interval',
-            'function': function() {
+            title: 'Set interval',
+            function: function () {
 
             },
         },
         {
-            'title': 'Set packet size',
-            'function': function() {
+            title: 'Set packet size',
+            function: function () {
 
             },
         },
         {
-            'title': 'Run upload test (from device)',
-            'function': function() {
+            title: 'Run upload test (from device)',
+            function: function () {
 
             },
         },
         {
-            'title': 'Run download (to device)',
-            'function': function() {
+            title: 'Run download (to device)',
+            function: async function () {
+                await stopTest();
 
+                await sendControlRequest({ op: 'start' });
+
+                seq = 0;
+
+                runTimer = setInterval(function() {
+                    let msg = Buffer.alloc(settings.size);
+                    for(let ii = 4; ii < settings.size; ii++) {
+                        msg.writeUInt8(ii % 256, ii);
+                    }
+                    msg.writeInt32LE(sendSeq++, 0);
+
+                    server.send(msg, remotePort, remoteAddr);
+                }, settings.interval);
             },
         },
+        {
+            title: 'Stop test',
+            function: stopTest,
+        }
     ];
 
-    while(true) {
+    const res = await sendControlRequest({ op: 'info' });
+    if (res) {
+        console.log('info', res);
+        remoteAddr = res.ip;
+        remotePort = res.port;        
+    }
+
+    while (true) {
         let titles = [];
-        for(const obj of testCommands) {
+        for (const obj of testCommands) {
             titles.push(obj.title);
         }
 
@@ -125,3 +180,21 @@ async function run() {
 
 run();
 
+//
+// UDP server
+//
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(settings.port);
